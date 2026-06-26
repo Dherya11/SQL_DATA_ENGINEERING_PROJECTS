7c9b510c5abf071a92096caf8c2e681353683bf0
Dherya bedi <nishubedi655@gmail.com>
2026-06-26 15:58:52 +0530

    w

--- LESSONS/Lessons/1.9/1_EDA/1.22_DDL_DML_pt2.sql
+++ LESSONS/Lessons/1.9/1_EDA/1.22_DDL_DML_pt2.sql
@@ -1,4 +1,5 @@
-CREATE TABLE staging.job_postings_fact AS   
+-- LESSONS/Lessons/1.9/1_EDA/1.22_DDL_DML_pt2.sql
+CREATE OR REPLACE TABLE staging.job_postings_fact AS   
 SELECT
     jpf.job_id,
     jpf.job_title_short,
@@ -20,5 +21,78 @@ FROM data_jobs.job_postings_fact AS jpf
 LEFT JOIN data_jobs.company_dim AS cd
     ON jpf.company_id = cd.company_id;
 
-    SELECT COUNT(*)
-    FROM staging.job_postings_fact;
+SELECT COUNT(*)
+FROM staging.job_postings_fact;
+
+CREATE OR REPLACE VIEW staging.priority_jobs_flat_view AS   
+SELECT 
+     jpf.*
+FROM staging.job_postings_fact as jpf
+JOIN staging.priority_roles AS r
+     ON jpf.job_title_short = r.role_name 
+     WHERE r.priority_lvl = 1;
+
+SELECT
+   job_title_short,
+   COUNT(*) AS job_count
+FROM staging.priority_jobs_flat_view
+GROUP BY job_title_short 
+ORDER BY job_count DESC;
+
+
+CREATE TEMPORARY TABLE senior_job_flat_temp AS 
+SELECT * 
+FROM staging.priority_jobs_flat_view
+WHERE job_title_short = 'Senior Data Engineer';
+
+SELECT
+   job_title_short,
+   COUNT(*) AS job_count
+FROM senior_job_flat_temp
+GROUP BY job_title_short 
+ORDER BY job_count DESC;
+
+SELECT COUNT (*) FROM Staging.job_postings_fact;
+SELECT COUNT (*) FROM Staging.priority_jobs_flat_view;
+SELECT COUNT (*) FROM senior_job_flat_temp;
+
+DELETE FROM staging.job_postings_fact
+WHERE job_posted_date < '2024-01-01';
+
+SELECT COUNT (*) FROM Staging.job_postings_fact;
+SELECT COUNT (*) FROM Staging.priority_jobs_flat_view;
+SELECT COUNT (*) FROM senior_job_flat_temp;
+
+TRUNCATE TABLE staging.job_postings_fact;
+
+
+SELECT * FROM staging.job_postings_fact;
+
+INSERT INTO staging.job_postings_fact
+SELECT
+    jpf.job_id,
+    jpf.job_title_short,
+    jpf.job_title,
+    jpf.job_location,
+    jpf.job_via,
+    jpf.job_schedule_type,
+    jpf.job_work_from_home,
+    jpf.search_location,
+    jpf.job_posted_date,
+    jpf.job_no_degree_mention,
+    jpf.job_health_insurance,
+    jpf.job_country,
+    jpf.salary_rate,
+    jpf.salary_year_avg,
+    jpf.salary_hour_avg,
+    cd.name                AS company_name,
+FROM data_jobs.job_postings_fact AS jpf
+LEFT JOIN data_jobs.company_dim AS cd
+    ON jpf.company_id = cd.company_id
+    WHERE job_posted_date < '2024-01-01';
+
+
+SELECT COUNT (*) FROM Staging.job_postings_fact;
+SELECT COUNT (*) FROM Staging.priority_jobs_flat_view;
+SELECT COUNT (*) FROM senior_job_flat_temp;
+
