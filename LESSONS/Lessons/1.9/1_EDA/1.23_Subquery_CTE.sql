7428f448dac8ad2957452e7c7765a33318a16442
Dherya bedi <nishubedi655@gmail.com>
2026-06-29 15:42:20 +0530

    .

--- LESSONS/Lessons/1.9/1_EDA/1.23_Subquery_CTE.sql
+++ LESSONS/Lessons/1.9/1_EDA/1.23_Subquery_CTE.sql
@@ -78,6 +78,7 @@ LIMIT 10;
 
 -- Question: Group jobs by job_title_short. Keep only job titles where the median salary 
 -- of that group is higher than the median salary of just remote jobs
+
 SELECT 
     job_title_short,
     MEDIAN(salary_year_avg) AS median_salary
@@ -88,3 +89,79 @@ HAVING MEDIAN(salary_year_avg) > (
     FROM job_postings_fact
     WHERE job_work_from_home = TRUE
     );
+
+-- CTE Example
+-- Compare how much (or less) remote roles pays compared to onsite roles for each job title.
+-- Use a CTE to calculate the median salary by title and work arrangement, then compare those medians.
+
+WITH title_median AS (
+    SELECT 
+        job_title_short,
+        job_work_from_home,
+        MEDIAN(salary_year_avg) ::INT AS median_salary
+    FROM job_postings_fact 
+    WHERE job_country = 'India'
+    GROUP BY
+        job_title_short,
+        job_work_from_home
+)
+
+SELECT 
+   r.job_title_short,
+   r.median_salary AS remote_median_salary,
+   o.median_salary AS onsite_median_salary,
+   (r.median_salary - o.median_salary) AS remote_premium
+FROM title_median AS r
+INNER JOIN title_median AS o
+    ON r.job_title_short = o.job_title_short
+WHERE r.job_work_from_Home = TRUE
+    AND o.job_work_from_home = FALSE
+ORDER BY remote_premium DESC;    
+
+-- SOURCE TABLE AND TARGET TABLE 
+SELECT *
+FROM range(3) as trc(key) ;
+
+SELECT *
+FROM range(3) as tgt(key) ;
+
+SELECT *
+FROM range(3) as Src(key) 
+WHERE EXISTS(
+   SELECT 1
+   FROM range(2) as tgt(key)
+   WHERE tgt.key = src.key
+
+);
+
+SELECT *
+FROM range(3) as Src(key) 
+WHERE NOT EXISTS(
+   SELECT 1
+   FROM range(2) as tgt(key)
+   WHERE tgt.key = src.key
+
+);
+
+-- Final Example 
+-- Identify Job postings that have no associated skills before loading them into data mart
+
+SELECT * 
+FROM job_postings_fact
+ORDER BY job_id
+LIMIT 10;
+
+SELECT * 
+FROM skills_job_dim
+ORDER BY job_id
+LIMIT 40;
+
+SELECT * 
+FROM job_postings_fact AS tgt
+WHERE NOT EXISTS(
+    SELECT * 
+    FROM skills_job_dim AS src
+    WHERE tgt.job_id = src.job_id
+)
+ORDER BY job_id;
+
