72020c491a9508d604a7972a76156ee785da0667
Dherya bedi <nishubedi655@gmail.com>
2026-07-04 15:18:11 +0530

    j

---
+++ LESSONS/priority_jobs_snapshot-INITIAL.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE TABLE main.priority_jobs_snapshot AS(
+SELECT
+    job_id             INTEGER PRIMARY KEY ,
+    job_title_short     VARCHAR,
+    company_name        VARCHAR,
+    job_posted_date     TIMESTAMP,
+    salary_year_avg     DOUBLE,
+    priority_lvl        INTEGER,
+    updated_at          TIMESTAMP
+   ); 
+INSERT INTO main.priority_jobs_snapshot(
+    job_id,
+    job_title_short,
+    company_name,
+    job_posted_date,
+    salary_year_avg,
+    priority_lvl,
+    updated_at
+)
+SELECT 
+jpf.job_id,
+jpf.job_title_short,
+cd.name AS company_name,
+jpf.job_posted_date,
+jpf.salary_year_avg,
+r.priority_lvl,
+CURRENT_TIMESTAMP;
+
