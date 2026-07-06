a688c2e0921545f827a60e3eb2db13ca23d31c3b
Dherya bedi <nishubedi655@gmail.com>
2026-07-06 15:07:48 +0530

    m

--- LESSONS/priority_jobs_snapshot-INITIAL.sql
+++ LESSONS/priority_jobs_snapshot-INITIAL.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE TABLE main.priority_jobs_snapshot AS(
-SELECT
-    job_id             INTEGER PRIMARY KEY ,
+-- LESSONS/priority_jobs_snapshot-INITIAL.sql
+CREATE OR REPLACE TABLE main.priority_jobs_snapshot(
+    job_id              INTEGER ,
     job_title_short     VARCHAR,
     company_name        VARCHAR,
     job_posted_date     TIMESTAMP,
@@ -24,5 +24,18 @@ cd.name AS company_name,
 jpf.job_posted_date,
 jpf.salary_year_avg,
 r.priority_lvl,
-CURRENT_TIMESTAMP;
+CURRENT_TIMESTAMP
+FROM 
+    data_jobs.job_postings_fact AS jpf
+LEFT JOIN data_jobs.company_dim AS cd
+    ON jpf.company_id = cd.company_id
+INNER JOIN staging.priority_roles AS r
+    ON jpf.job_title_short = r.role_name;
 
+SELECT job_title_short,
+       COUNT(*) AS job_count,
+       MIN(priority_lvl) AS priority_lvl,
+       MIN(updated_at) AS updated_at
+FROM priority_jobs_snapshot
+GROUP BY job_title_short
+ORDER BY job_count DESC;
