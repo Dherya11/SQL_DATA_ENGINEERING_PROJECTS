36f47af10fe32ed7b786f57470368510168e606a
Dherya bedi <nishubedi655@gmail.com>
2026-07-06 14:50:18 +0530

    ,

--- LESSONS/1.24_priority_roles.sql
+++ LESSONS/1.24_priority_roles.sql
@@ -1,5 +1,5 @@
 -- LESSONS/1.24_priority_roles.sql
-CREATE TABLE staging.priority_roles (
+CREATE OR REPLACE TABLE staging.priority_roles (
     role_id     INTEGER PRIMARY KEY,
     role_name   VARCHAR,
     priority_lvl INTEGER
@@ -9,7 +9,7 @@ INSERT INTO staging.priority_roles (role_id, role_name, priority_lvl)
 VALUES(
     (1, 'Data Engineer',        1), 
     (2, 'Senior Data Engineer',   1), 
-    (3, 'Data Engineer',         3);
+    (3, 'Software Engineer',         3);
 
 
 SELECT * FROM staging.priority_roles;
