2a5fd71e38b739c56e3c056ed71781d891d43b0d
Dherya bedi <nishubedi655@gmail.com>
2026-07-09 15:38:23 +0530

    THE DLCs

--- LESSONS/1.24_Case_Expressions.sql
+++ LESSONS/1.24_Case_Expressions.sql
@@ -27,4 +27,22 @@ SELECT
         ELSE 'High'
     END AS salary_category
 FROM job_postings_fact
-LIMIT 10;      
+LIMIT 10;   
+
+-- Categorizing Categorical Values 
+-- Categorizing the 'job_title' column values as;
+   -- 'Data Analyst'
+   -- 'Data Engineer'
+   -- 'Data Scientist'
+
+SELECT 
+    job_title_short,
+    CASE 
+        WHEN job_title LIKE '%Data%' AND job_title LIKE '%Analyst%' THEN 'Data Analyst'
+        WHEN job_title LIKE '%Data%' AND job_title LIKE '%Engineer%' THEN 'Data Engineer'
+        WHEN job_title LIKE '%Data%' AND job_title LIKE '%Scientist%' THEN 'Data Scientist'
+        ELSE 'Other'
+    END AS job_title_category, job_title_short
+FROM job_postings_fact
+ORDER BY RANDOM()
+LIMIT 20;    
