11b3d919f2449eac7472587f60d2d77f2acbf3da
Dherya bedi <nishubedi655@gmail.com>
2026-07-17 16:00:25 +0530

    GG

--- LESSONS/1.27_Set_Operators.sql
+++ LESSONS/1.27_Set_Operators.sql
@@ -6,6 +6,31 @@ SELECT UNNEST([1, 1, 1, 2])
 EXCEPT ALL
 SELECT UNNEST([1, 1, 3]);
 
-DESCRIBE
+CREATE TEMP TABLE jobs_2023 AS 
 SELECT * EXCLUDE(job_id, job_posted_date)
-FROM job_postings_fact;
+FROM job_postings_fact
+WHERE EXTRACT(YEAR FROM job_posted_date) = 2023;
+
+SELECT * FROM jobs_2023;
+
+CREATE TEMP TABLE jobs_2024 AS 
+SELECT * EXCLUDE(job_id, job_posted_date)
+FROM job_postings_fact
+WHERE EXTRACT(YEAR FROM job_posted_date) = 2024;
+
+SELECT * FROM jobs_2024;
+
+-- Which unique job postings appeared in either 2023 or 2024 ?
+SELECT 
+    'jobs_2023' AS table_name,
+    COUNT(*) 
+FROM jobs_2023
+UNION
+SELECT
+    'jobs_2024' AS table_name,
+    COUNT(*) 
+FROM jobs_2024;
+
+SELECT * FROM jobs_2023
+UNION
+SELECT * FROM jobs_2024;
