87c9840e661c5faff364519b18fc9240d526e30a
Dherya bedi <nishubedi655@gmail.com>
2026-07-19 15:03:59 +0530

    xth

--- LESSONS/1.28_Test_Null_Functions.sql
+++ LESSONS/1.28_Test_Null_Functions.sql
@@ -12,3 +12,34 @@ SELECT SUBSTRING('SQL',2, 1);
 
 SELECT CONCAT('SQL', '-', 'Functions');
 
+SELECT TRIM(' SQL ');
+
+SELECT REPLACE('SQL', 'Q', '_');
+
+SELECT REGEXP_REPLACE('data.bro@gamil.com', '^.*(@)', '\1');
+
+
+-- Final Example - Cleanup this using Test functions
+WITH title_lower AS(
+    SELECT 
+        job_title,
+        LOWER(TRIM(job_title)) AS job_title_clean
+    FROM 
+        job_postings_fact    
+
+)
+
+SELECT 
+    job_title, 
+    CASE 
+        WHEN job_title_clean LIKE '%data%'
+         AND job_title_clean LIKE '%analyst%' THEN 'Data Analyst'
+        WHEN job_title_clean LIKE '%data%'
+         AND job_title_clean LIKE '%scientist%' THEN 'Data Scientist'
+        WHEN job_title_clean LIKE '%data%'
+         AND job_title_clean LIKE '%engineer%' THEN 'Data Engineer'
+        ELSE 'Other'
+    END AS job_title_company
+FROM title_lower
+ORDER BY RANDOM()
+LIMIT 30;
