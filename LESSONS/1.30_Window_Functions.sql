1a05eb27ff1023f06c21098bffe0379abd3c7e87
Dherya bedi <nishubedi655@gmail.com>
2026-07-24 14:15:34 +0530

    t

--- LESSONS/1.30_Window_Functions.sql
+++ LESSONS/1.30_Window_Functions.sql
@@ -12,3 +12,13 @@ SELECT
     COUNT(*) OVER ()
 FROM 
     job_postings_fact;    
+
+-- PARTITION BY - Find hourly salary
+SELECT 
+    job_id, 
+    job_title_short,
+    salary_hour_avg,
+    AVG(salary_hour_avg) OVER ()
+FROM job_postings_facts
+LIMIT 10 ;    
+        
